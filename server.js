c7e4b9ac2a083c744a84895153270f0658ccc2de
Mainak Dey <mainakdeykol@gmail.com>
2023-11-14 13:57:51 +0530

    my changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const cors = require('cors');
 const corsOptions = {
     origin: process.env.ALLOWED_CLIENTS.split(',')
-     ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+     //['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
   }
 
 const connectDB=require('./config/db');
